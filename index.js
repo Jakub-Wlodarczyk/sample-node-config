5c6fe9e600ca548a2e8612e1ba209cc29dcf0e2c
Jakub-Wlodarczyk <wlodarczyk.jakub.b@gmail.com>
2018-02-11 11:40:19 +0100

    chore: add main js file

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const server = require('http').Server(app);
+const port = 3000;
+const helmet = require('helmet');
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+	next();
+});
+
+//pre-flight requests
+app.options('*', function(req, res) {
+	res.send(200);
+});
+
+server.listen(port, (err) => {
+	if (err) {
+		throw err;
+	}
+	/* eslint-disable no-console */
+	console.log('Node Endpoints working :)');
+});
+
+module.exports = server;
+
